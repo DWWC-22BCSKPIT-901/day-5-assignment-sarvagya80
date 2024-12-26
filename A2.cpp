ccca7607ad362b984c38cbdd1088d73dc2ed8af9
sarvagya80 <adityapathak8081@gmail.com>
2024-12-26 10:56:49 +0530

    Create A2.cpp

---
+++ A2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> sortedSquares(vector<int>& nums) {
+    for (int i = 0; i < nums.size(); i++) {
+        nums[i] *= nums[i];
+    }
+    sort(nums.begin(), nums.end());
+    return nums;
+}
+
+int main() {
+    vector<int> nums = {-4, -1, 0, 3, 10};
+    vector<int> result = sortedSquares(nums);
+    cout << "Squares of sorted array: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+    return 0;
+}
